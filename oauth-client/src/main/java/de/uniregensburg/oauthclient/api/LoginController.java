d22b932ce3df9bf5f113bc46dec55483e272dcec
Julian Bauer <julian.bauer@cationstudio.com>
2022-05-10 10:44:22 +0000

    Save state and code verifier before redirect

--- oauth-client/src/main/java/de/uniregensburg/oauthclient/api/LoginController.java
+++ oauth-client/src/main/java/de/uniregensburg/oauthclient/api/LoginController.java
@@ -1,11 +1,14 @@
 package de.uniregensburg.oauthclient.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
+import de.uniregensburg.oauthclient.model.State;
+import de.uniregensburg.oauthclient.repository.StateRepository;
 import de.uniregensburg.oauthclient.util.CodeChallengeMethod;
 import de.uniregensburg.oauthclient.util.PKCE;
 import de.uniregensburg.oauthclient.util.PKCEException;
@@ -22,17 +25,21 @@ public class LoginController {
 	private String authorizationEndpoint;
     @Value("${redirect_uri}")
     private String redirectUri;
+    @Autowired
+    private StateRepository stateRepository;
 
     @GetMapping("/webapp/login")
     public RedirectView login(RedirectAttributes attributes) {
         String responseType = ResponseType.CODE.toString(); // TODO
         String codeChallengeMethod = CodeChallengeMethod.S256.toString();
-        String state = "asdfkl"; // TODO
+        State state = new State("http://localhost:8080/login"); // TODO 
         String scope = "photos"; // TODO
 
         String codeChallenge = null;
         try {
             String codeVerifier = PKCE.getCodeVerifier();
+            state.setCodeVerifier(codeVerifier);
+            stateRepository.save(state);
             codeChallenge = PKCE.generateCodeChallenge(codeVerifier);
         } catch (PKCEException e) {
             // TODO Redirect to error page
@@ -43,7 +50,7 @@ public class LoginController {
         attributes.addAttribute("client_id", clientId);
         attributes.addAttribute("redirect_uri", redirectUri);
         attributes.addAttribute("scope", scope);
-        attributes.addAttribute("state", state);
+        attributes.addAttribute("state", state.getId().toString());
         attributes.addAttribute("code_challenge", codeChallenge);
         attributes.addAttribute("code_challenge_method", codeChallengeMethod);
         return new RedirectView(authorizationEndpoint);
