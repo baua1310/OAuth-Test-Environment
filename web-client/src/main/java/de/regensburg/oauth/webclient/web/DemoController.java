3bd5c0d0616d46ee296da1b4ce44b4576e7ca9d5
Julian Bauer <julian.bauer@cationstudio.com>
2022-06-18 16:37:19 +0000

    Removed unused import

--- web-client/src/main/java/de/regensburg/oauth/webclient/web/DemoController.java
+++ web-client/src/main/java/de/regensburg/oauth/webclient/web/DemoController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import javax.servlet.http.HttpServletRequest;
-
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 /*
