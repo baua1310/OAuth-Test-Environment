9f002ab77d1184e778790ca42b6886bcf2bca7de
Julian Bauer <julian.bauer@cationstudio.com>
2022-07-27 14:08:15 +0200

    document resource server

--- resource-server/src/main/java/de/regensburg/oauth/resourceserver/config/ResourceServerConfig.java
+++ resource-server/src/main/java/de/regensburg/oauth/resourceserver/config/ResourceServerConfig.java
@@ -4,22 +4,29 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-/*
- * Source: https://github.com/Baeldung/spring-security-oauth/tree/master/oauth-authorization-server
- */
-
 @EnableWebSecurity
 public class ResourceServerConfig {
 
+    /** 
+    * 
+    * 
+    * Source: https://github.com/Baeldung/spring-security-oauth/blob/master/oauth-authorization-server/resource-server/src/main/java/com/baeldung/config/ResourceServerConfig.java
+    * Source: https://github.com/spring-projects/spring-authorization-server/blob/main/samples/messages-resource/src/main/java/sample/config/ResourceServerConfig.java
+    *
+    * Modified: path and scope
+    * 
+    * @param    http
+    * @return   security filter chain for demo path
+    */
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.mvcMatcher("/demo/**")
-          .authorizeRequests()
-          .mvcMatchers("/demo/**")
-          .access("hasAuthority('SCOPE_demo')")
-          .and()
-          .oauth2ResourceServer()
-          .jwt();
+            .authorizeRequests()
+            .mvcMatchers("/demo/**")
+            .access("hasAuthority('SCOPE_demo')")
+            .and()
+            .oauth2ResourceServer()
+            .jwt();
         return http.build();
     }
 }
--- resource-server/src/main/java/de/regensburg/oauth/resourceserver/web/DemoController.java
+++ resource-server/src/main/java/de/regensburg/oauth/resourceserver/web/DemoController.java
@@ -3,13 +3,20 @@ package de.regensburg.oauth.resourceserver.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*
- * Source: https://github.com/Baeldung/spring-security-oauth/tree/master/oauth-authorization-server
- */
-
 @RestController
 public class DemoController {
 
+    /** 
+    * Get demo resource
+    * 
+    * Source: https://github.com/Baeldung/spring-security-oauth/blob/master/oauth-authorization-server/resource-server/src/main/java/com/baeldung/web/ArticlesController.java
+    * Source: https://github.com/spring-projects/spring-authorization-server/blob/main/samples/messages-resource/src/main/java/sample/web/MessagesController.java
+    *
+    * Modified: path, return type, returned content
+    * 
+    * @param    http
+    * @return   security filter chain for demo path
+    */
     @GetMapping("/demo")
     public String getDemo() {
         return "Demo works!";
